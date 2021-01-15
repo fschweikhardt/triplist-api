5587bd0c4e1251734a14a01f017faa2c5501c920
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-14 20:31:11 -0500

    updates

--- src/users/UsersRouter.js
+++ src/users/UsersRouter.js
@@ -2,6 +2,8 @@ const express = require('express')
 const UsersRouter = express.Router()
 const bodyParser = express.json()
 const UsersService = require('./UsersService')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 //const logger = require('../logger.js)
 //const xss = require('xss')
 
@@ -27,13 +29,14 @@ UsersRouter
 UsersRouter
     .route('/api/user')
     .post(bodyParser, (req,res,next) => {
-        const { username, password } = req.body
+        const { username } = req.body
         UsersService.getUser(req.app.get('db'), username)
             .then(user => {
                 res.json(user).status(201)
             })
             .catch(next)
-})      
+})     
+
 UsersRouter
     .route('/api/userLists')
     .post(bodyParser, (req,res,next) => {
@@ -55,5 +58,64 @@ UsersRouter
             })
             .catch(next)
 })
+
+
+UsersRouter
+    .route('/api/register')
+    .post(bodyParser, (req,res,next) => {
+        const { email, username, password } = req.body
+        bcrypt.hash(password, 2)
+        const new_user = { email, username, password}
+        UsersService.newUser(req.app.get('db'), new_user)
+            .then(data => {
+                res.json(data).status(201)
+            })
+            .catch(next)
+    })
+
+UsersRouter
+    .route('/api/login')
+    .post(bodyParser, (req,res,next) => {
+        const { username, password } = req.body
+        bcrypt.hash(password, 2)   
+    .then(data => {
+        res.json(data)
+        console.log(data)
+        })
+
+
+
+
+        // const jwtKey = "my_secret_key"
+        // const jwtExpirySeconds = 300
+
+        // const { username, password } = req.body
+       
+        // if (!username || !password || users[username] !== password) {
+		// // return 401 error is username or password doesn't exist, or if password does
+		// // not match the password in our records
+		// return res.status(401).end()
+    	// }
+
+        // // Create a new token with the username in the payload
+        // // and which expires 300 seconds after issue
+        // const token = jwt.sign({ username }, jwtKey, {
+        //     algorithm: "HS256",
+        //     expiresIn: jwtExpirySeconds,
+        // })
+        // console.log("token:", token)
+        
+        // // set the cookie as the token string, with a similar max age as the token
+        // // here, the max age is in milliseconds, so we multiply by 1000
+        // res.cookie("token", token, { maxAge: jwtExpirySeconds * 1000 })
+        // res.end()
+        // const { username, password } = req.body
+        // UsersService.getUser(req.app.get('db'), username)
+        //     .then(user => {
+        //         res.json(user).status(201)
+        //     })
+        //     .catch(next)
+})   
+
 
 module.exports = UsersRouter
