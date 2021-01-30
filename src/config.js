cae916832c6d005afd5953bc99efb49c49d3dc37
fschweikhardt <schweikhardt.web@gmail.com>
2021-01-29 22:17:41 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
-    PORT: process.env.PORT,
-    NODE_ENV: process.env.NODE_ENV,
-    DATABASE_URL: process.env.DATABASE_URL,
-    API_TOKEN: process.env.API_TOKEN,
+    PORT: process.env.PORT || 8000,
+    //NODE_ENV: process.env.NODE_ENV || production,
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://postgres:postgres@localhost:5432/triplist',
+    API_TOKEN: process.env.API_TOKEN || 123456789,
     JWT_SECRET: process.env.JWT_SECRET
   }
