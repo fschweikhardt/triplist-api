574825ada7e39db1f266be3ad6aab5df643388f2
fschweikhardt <schweikhardt.web@gmail.com>
2021-04-27 06:52:00 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,24 +1,19 @@
 const knex = require('knex')
 const app = require('./app')
-const { PORT, DATABASE_URL } = require('./config')
+const { PORT } = require('./config')
 
 const db = knex({
   client: 'pg',
-  connection: DATABASE_URL
+  connection: {
+      host : 'ec2-34-192-173-173.compute-1.amazonaws.com',
+      user: 'jzwvtqvssnlczw',
+      database : 'd70asjqg3c6hka',
+      password : '41cfbf98bb1916b867189c19b45f37e2545ef6e33446b6bc3daaa74e583db5b6',
+      ssl: { 
+        rejectUnauthorized: false 
+      }
+    }
 })
-
-// const db = knex({
-//   client: 'pg',
-//   connection: {
-//       host : 'ec2-18-233-83-165.compute-1.amazonaws.com',
-//       user: 'zkwnrvbuimcdel',
-//       database : 'd3qj30j6aiscv',
-//       password : '3ba026693655b972b960d98011aa6cf586edafd4478c834a229722864f4e4e80',
-//       ssl: { 
-//         rejectUnauthorized: false 
-//       }
-//     }
-// })
 
 app.set('db', db)
 
